a3464e8fd805873c91adaaec77fb032fb8ba1473
fvilarin <fvilarin@akamai.com>
2024-06-04 16:08:00 -0300

    Provisioning of Akamai Datastream 2 and also the resources (Project, Table and Transform) in Hydrolix.

--- iac/hydrolix.tf
+++ iac/hydrolix.tf
@@ -1,18 +1,29 @@
+# Definition of local variables.
+locals {
+  hydrolixHostname              = "${var.settings.hydrolix.prefix}.${var.settings.general.domain}"
+  hydrolixUrl                   = "https://${local.hydrolixUrl}"
+  hydrolixOriginHostname        = "origin-${local.hydrolixHostname}"
+  hydrolixOperatorUrl           = "https://www.hydrolix.io/operator/latest/operator-resources?namespace=${var.settings.hydrolix.namespace}"
+  hydrolixInstallScriptFilename = "../bin/hydrolix/install.sh"
+  hydrolixOriginScriptFilename  = "../bin/hydrolix/fetchOrigin.sh"
+}
+
 # Downloads the Hydrolix operator content.
 data "http" "hydrolixOperator" {
-  url = "https://www.hydrolix.io/operator/latest/operator-resources?namespace=${var.settings.hydrolix.namespace}"
+  url    = local.hydrolixOperatorUrl
+  method = "GET"
 }
 
 # Saves the Hydrolix operator file.
 resource "local_sensitive_file" "hydrolixOperator" {
-  filename   = var.settings.hydrolix.operatorFilename
+  filename   = abspath(pathexpand(var.settings.hydrolix.operatorFilename))
   content    = data.http.hydrolixOperator.response_body
   depends_on = [ data.http.hydrolixOperator ]
 }
 
 # Saves the Hydrolix manifest file.
 resource "local_sensitive_file" "hydrolixManifest" {
-  filename = var.settings.hydrolix.manifestFilename
+  filename = abspath(pathexpand(var.settings.hydrolix.manifestFilename))
   content  = <<EOT
 ---
 apiVersion: hydrolix.io/v1
@@ -22,7 +33,7 @@ metadata:
   namespace: ${var.settings.hydrolix.namespace}
 spec:
   hydrolix_name: hdx
-  hydrolix_url: https://${local.hydrolixHost}
+  hydrolix_url: ${local.hydrolixUrl}
   admin_email: ${var.settings.general.email}
   db_bucket_region: ${data.linode_object_storage_cluster.hydrolix.id}
   db_bucket_url: https://${linode_object_storage_bucket.hydrolix.hostname}
@@ -49,21 +60,35 @@ EOT
 resource "null_resource" "deployHydrolix" {
   provisioner "local-exec" {
     environment = {
+      KUBECONFIG=abspath(pathexpand(var.settings.hydrolix.kubeconfigFilename))
       NAMESPACE=var.settings.hydrolix.namespace
-      CONFIGURATION_FILENAME=var.settings.hydrolix.configurationFilename
-      CERTIFICATE_KEY_FILENAME=var.settings.general.certificate.keyFilename
-      CERTIFICATE_PEM_FILENAME=var.settings.general.certificate.pemFilename
-      OPERATOR_FILENAME=var.settings.hydrolix.operatorFilename
-      MANIFEST_FILENAME=var.settings.hydrolix.manifestFilename
+      OPERATOR_FILENAME=abspath(pathexpand(var.settings.hydrolix.operatorFilename))
+      MANIFEST_FILENAME=abspath(pathexpand(var.settings.hydrolix.manifestFilename))
+      CERTIFICATE_KEY_FILENAME=abspath(pathexpand(var.settings.general.certificate.keyFilename))
+      CERTIFICATE_PEM_FILENAME=abspath(pathexpand(var.settings.general.certificate.pemFilename))
     }
 
     quiet   = true
-    command = "./deployHydrolix.sh"
+    command = abspath(pathexpand(local.hydrolixInstallScriptFilename))
   }
 
   depends_on = [
+    local_sensitive_file.hydrolixKubeconfig,
     local_sensitive_file.hydrolixOperator,
-    local_sensitive_file.hydrolixManifest,
-    local_sensitive_file.hydrolixConfiguration
+    local_sensitive_file.hydrolixManifest
+  ]
+}
+
+# Fetches the Hydrolix origin.
+data "external" "hydrolixOrigin" {
+  program    = [ abspath(pathexpand(local.hydrolixOriginScriptFilename)) ]
+  query      = {
+    KUBECONFIG=abspath(pathexpand(var.settings.hydrolix.kubeconfigFilename))
+    NAMESPACE=var.settings.hydrolix.namespace
+  }
+  depends_on = [
+    local_sensitive_file.hydrolixKubeconfig,
+    linode_lke_cluster.hydrolix,
+    null_resource.deployHydrolix,
   ]
 }
