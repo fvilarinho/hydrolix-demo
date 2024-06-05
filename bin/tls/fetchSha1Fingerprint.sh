a2d699036148607f46769121e3a9edb405f50050
fvilarin <fvilarin@akamai.com>
2024-06-04 21:44:09 -0300

    Provisioning of Akamai Datastream 2 and also the resources (Project, Table and Transform) in Hydrolix.

--- bin/tls/fetchSha1Fingerprint.sh
+++ bin/tls/fetchSha1Fingerprint.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# Prepares the environment to execute this script.
-function prepareToExecute() {
-  cd ../../ || exit 1
-
-  source functions.sh
-}
-
 # Fetches the SHA1 fingerprint using the PEM file.
 function fetchSha1Fingerprint() {
-  FINGERPRINT=$($OPENSSL_CMD x509 -noout -fingerprint -in "$CERTIFICATE_PEM_FILENAME" | sha1sum | awk -F' ' '{print $1}')
+  export CERTIFICATE_PEM_FILENAME="$1"
+
+  FINGERPRINT=$($OPENSSL_CMD x509 \
+                             -noout \
+                             -fingerprint \
+                             -in "$CERTIFICATE_PEM_FILENAME" | sha1sum | awk -F' ' '{print $1}')
 
   # Returns to terraform the fingerprint.
   echo "{\"fingerprint\": \"$FINGERPRINT\"}"
 }
 
-# Main function.
-function main() {
-  prepareToExecute
-  fetchSha1Fingerprint
-}
-
-main
+fetchSha1Fingerprint "$1"
