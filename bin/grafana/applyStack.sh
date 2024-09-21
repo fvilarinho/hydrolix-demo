d7975f54d3cd20c7f9beabe731a746a08feec024
Felipe Vilarinho <fvilarinho@gmail.com>
2024-09-21 10:30:52 -0300

    Code improvements.

--- bin/grafana/applyStack.sh
+++ bin/grafana/applyStack.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# Creates the namespace of the stack.
 function createNamespace() {
   $KUBECTL_CMD create namespace "$NAMESPACE" \
                -o yaml \
                --dry-run=client | $KUBECTL_CMD apply -f -
 }
 
+# Creates the settings of the stack.
 function createSettings() {
+  # Ingress definitions.
   $KUBECTL_CMD create configmap ingress-settings \
                --from-file="$INGRESS_SETTINGS_FILENAME" \
                -n "$NAMESPACE" \
                -o yaml \
                --dry-run=client | $KUBECTL_CMD apply -f -
 
+  # Certificate definitions.
   $KUBECTL_CMD create configmap ingress-tls-certificate \
                --from-file="$CERTIFICATE_FILENAME" \
                -n "$NAMESPACE" \
@@ -25,10 +29,12 @@ function createSettings() {
                --dry-run=client | $KUBECTL_CMD apply -f -
 }
 
+# Apply the stack.
 function applyStack() {
   $KUBECTL_CMD apply -f "$STACK_FILENAME" -n "$NAMESPACE"
 }
 
+# Main function.
 function main() {
   createNamespace
   createSettings
--- bin/grafana/fetchOrigin.sh
+++ bin/grafana/fetchOrigin.sh
@@ -7,8 +7,9 @@ function fetchOrigin() {
 
   HOSTNAME=
 
+  # Waits until LKE cluster load balancer is ready.
   while true; do
-    # Fetches the LKE cluster loadbalancer hostname.
+    # Fetches the LKE cluster load balancer hostname.
     HOSTNAME=$($KUBECTL_CMD get service ingress \
                             -n "$NAMESPACE" \
                             -o json | $JQ_CMD -r '.status.loadBalancer.ingress[].hostname')
@@ -20,7 +21,7 @@ function fetchOrigin() {
     sleep 5
   done
 
-  # Returns to terraform the hostname.
+  # Returns the fetched hostname.
   echo "{\"hostname\": \"$HOSTNAME\"}"
 }
 
