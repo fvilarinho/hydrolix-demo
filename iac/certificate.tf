eb6b9ce36beb2145a7514b0b496f6af861e8017c
Felipe Vilarinho <fvilarinho@gmail.com>
2024-09-21 11:17:27 -0300

    Code improvements.

--- iac/certificate.tf
+++ iac/certificate.tf
@@ -1,3 +1,4 @@
+# Required variables.
 locals {
   certificateIssuanceScript              = abspath(pathexpand("../bin/tls/certificateIssuance.sh"))
   certificateIssuanceCredentialsFilename = abspath(pathexpand("../etc/tls/certificateIssuance.properties"))
@@ -5,15 +6,18 @@ locals {
   certificateKeyFilename                 = abspath(pathexpand("../etc/tls/privkey.pem"))
 }
 
-resource "local_sensitive_file" "certificateValidationCredentials" {
+# Creates the certificate issuance credentials.
+resource "local_sensitive_file" "certificateIssuanceCredentials" {
   filename = local.certificateIssuanceCredentialsFilename
   content  = <<EOT
 dns_linode_key = ${var.credentials.linodeToken}
 EOT
 }
 
+# Issues the certificate using Certbot.
 resource "null_resource" "certificateIssuance" {
   provisioner "local-exec" {
+    # Required variables.
     environment = {
       CERTIFICATE_ISSUANCE_CREDENTIALS_FILENAME = local.certificateIssuanceCredentialsFilename
       DOMAIN                                    = var.settings.general.domain
@@ -24,15 +28,17 @@ resource "null_resource" "certificateIssuance" {
     command = local.certificateIssuanceScript
   }
 
-  depends_on = [ local_sensitive_file.certificateValidationCredentials ]
+  depends_on = [ local_sensitive_file.certificateIssuanceCredentials ]
 }
 
+# Saves the certificate to be used in the stacks.
 resource "local_sensitive_file" "certificate" {
   filename   = local.certificateFilename
   content    = file("/etc/letsencrypt/live/${var.settings.general.domain}/fullchain.pem")
   depends_on = [ null_resource.certificateIssuance ]
 }
 
+# Saves the certificate key to be used in the stacks.
 resource "local_sensitive_file" "certificateKey" {
   filename   = local.certificateKeyFilename
   content    = file("/etc/letsencrypt/live/${var.settings.general.domain}/privkey.pem")
