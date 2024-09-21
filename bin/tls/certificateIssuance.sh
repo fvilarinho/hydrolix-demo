31e92ac1985a0246f8ec7eeae66b0865cc4afe98
Felipe Vilarinho <fvilarinho@gmail.com>
2024-09-21 11:11:07 -0300

    Code improvements.

--- bin/tls/certificateIssuance.sh
+++ bin/tls/certificateIssuance.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 
-$CERTBOT_CMD certonly \
-             --dns-linode \
-             --dns-linode-credentials "$CERTIFICATE_ISSUANCE_CREDENTIALS_FILENAME" \
-             --dns-linode-propagation-seconds 300 \
-             --domain "$DOMAIN" \
-             --email "$EMAIL"
+# Check the dependencies of this script.
+function checkDependencies() {
+  if [ -z "$DOMAIN" ]; then
+    echo "The domain to be used in the certificate issuance is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+
+  if [ -z "$EMAIL" ]; then
+    echo "The email to be used in the certificate issuance is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+
+  if [ -z "$CERTIFICATE_ISSUANCE_CREDENTIALS_FILENAME" ]; then
+    echo "The credentials filename for the certificate issuance is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+
+  if [ -z "$CERTIFICATE_ISSUANCE_PROPAGATION_DELAY" ]; then
+    echo "The propagation delay for the certificate issuance is not defined! Please define it first to continue!"
+
+    exit 1
+  fi
+}
+
+# Issue the certificate using Certbot. The validation will be in the Linode DNS.
+function issueTheCertificate() {
+  $CERTBOT_CMD certonly \
+               --dns-linode \
+               --dns-linode-credentials "$CERTIFICATE_ISSUANCE_CREDENTIALS_FILENAME" \
+               --dns-linode-propagation-seconds "$CERTIFICATE_ISSUANCE_PROPAGATION_DELAY" \
+               --domain "$DOMAIN" \
+               --email "$EMAIL"
+}
+
+# Main function.
+function main() {
+  checkDependencies
+  issueTheCertificate
+}
+
+main
