7cddef8679ef40d109e306b0b9c537070ed8a250
fvilarinho <fvilarinho!@gmail.com>
2024-09-23 13:31:13 -0300

    Code improvements and bug fixes.

--- iac/grafana.tf
+++ iac/grafana.tf
@@ -1,8 +1,13 @@
 # Required variables.
 locals {
   grafanaKubeconfigFilename      = abspath(pathexpand("../etc/grafana/.kubeconfig"))
+  grafanaHostname                = "${var.settings.grafana.prefix}.${var.settings.general.domain}"
+  grafanaUrl                     = "https://${local.grafanaHostname}"
   grafanaIngressSettingsFilename = abspath(pathexpand("../etc/grafana/ingress.conf"))
   grafanaStackFilename           = abspath(pathexpand("../etc/grafana/stack.yaml"))
+  grafanaDatasourceFilename      = abspath(pathexpand("../etc/grafana/resources/datasource.json"))
   grafanaApplyStackScript        = abspath(pathexpand("../bin/grafana/applyStack.sh"))
+  grafanaApplyResourcesScript    = abspath(pathexpand("../bin/grafana/applyResources.sh"))
+  grafanaCleanUpScript           = abspath(pathexpand("../bin/grafana/cleanUp.sh"))
   fetchGrafanaOriginScript       = abspath(pathexpand("../bin/grafana/fetchOrigin.sh"))
 }
